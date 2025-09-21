3e77002e44c58aea976432e63af7270e1b630d96
Saulo de Tarso <saulo.detarso06@yahoo.com.br>
2025-09-21 16:42:09 -0300

    fix: corrigido caminho das rotas de autenticação e leitura do arquivo .env no server.js

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
+const { connectDB } = require("./backend/config/db");
+const { sequelize } = require("./backend/models/User");
+const authRoutes = require('./routes/authRoutes');
+
+sequelize.authenticate()
+    .then(() => console.log('✅ Conectado ao PostgreSQL!'))
+    .catch(err => console.error('❌ Erro ao conectar ao PostgreSQL:', err));
 
 const app = express();
-const PORT = 5000;
 
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// rotas
+// Rotas
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+connectDB(process.env.MONGO_URI).then(() => {
+    app.listen(PORT, () => console.log(`🚀 Server on http://localhost:${PORT}`));
 });
+
+
